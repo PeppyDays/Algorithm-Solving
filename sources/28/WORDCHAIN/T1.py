d78f3ce24c76c223cb6f9a6706e2e19e2ebc2c90
PeppyDays <peppydays@gmail.com>
2016-09-02 11:09:21 +0900

    WORDCHAIN, 노드의 셀프 간선(xox 문자열 같은)의 경우 문제 발생

--- sources/28/WORDCHAIN/T1.py
+++ sources/28/WORDCHAIN/T1.py
@@ -45,15 +45,48 @@ def check_graph_as_euler(in_degree, out_degree):
             'type': 'CIRCUIT',
         }
     elif len(in_out_diff) is 2 and set(in_out_diff.values()) == {1, -1}:
-        start_node = [k for k, v in in_out_diff.items() if v is -1][0]
+        ret_start_node = [k for k, v in in_out_diff.items() if v is -1][0]
         return True, {
             'type': 'TRAIL',
-            'start_node': start_node,
+            'start_node': ret_start_node,
         }
     else:
         return False, None
 
 
+def find_euler_circuit(here, adj_map):
+    """
+    오일러 서킷 혹은 트레일을 찾고, 노드가 아니라 간선을 역순으로 출력한다.
+    :param here: 현재 노드의 문자
+    :param adj_map: 현재-다음 노드 문자의 연결 관계
+    :return: 오일러 서킷 혹은 트레일을 돌았을 때의 간선 경로
+    """
+
+    circuit = []
+
+    # here 에 연결된 다음 there 노드를 찾아서 간선의 단어를 저장하고, there 로 이동해서 다시 dfs 를 수행한다.
+    # here 에 연결된 다음 노드가 다시 here 가 존재하는 경우 이 것 부터 실행해야 한다.
+    for there in adj_map.get(here, []):
+        if there == here:
+            continue
+
+        try:
+            v_word = adj_map[here][there].pop()
+        except IndexError:
+            continue
+
+        circuit = find_euler_circuit(there, adj_map)
+        circuit.append(v_word)
+
+    for self_word in adj_map.get(here, []).get(here, []):
+        circuit.append(self_word)
+
+    adj_map[here][here] = []
+
+    return circuit
+
+
+# 메인문 시작
 for t in range(int(input())):
     # Words: 입력받은 단어들
     Words = [input() for _ in range(int(input()))]
@@ -62,14 +95,18 @@ for t in range(int(input())):
     adj_map, in_degree, out_degree = make_graph(Words)
 
     # Euler 로 한붓그리기가 되는지 안되는지 판단하기
+    is_euler, euler_type = check_graph_as_euler(in_degree, out_degree)
 
+    # 만약 Euler 가 아니라면? IMPOSSIBLE 출력
+    if not is_euler:
+        print('IMPOSSIBLE')
+        continue
 
+    # Euler 를 순회 시작할 노드 찾기
+    start_node = euler_type.get('start_node', Words[0][0])
 
+    # Euler Circuit 순회
+    circuit = find_euler_circuit(start_node, adj_map)
+    circuit.reverse()
 
-
-
-
-
-
-
-
+    print(' '.join(circuit))
