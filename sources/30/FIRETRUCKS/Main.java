0a14d03d5f238ba500ecd1064c00d94916887d61
PeppyDays <peppydays@gmail.com>
2016-09-13 16:56:21 +0900

    ROUTING, NTHLON, FIRETRUCKS Java 문제풀이 추가

---
+++ sources/30/FIRETRUCKS/Main.java
@@ -0,0 +1,124 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    static int maxV = 1001;
+    static int maxE = maxV*(maxV-1)/2;
+    static int INF = Integer.MAX_VALUE;
+
+    static int V, E;
+    static int n, m;
+
+    @SuppressWarnings("unchecked")
+    static ArrayList<Pair>[] map = new ArrayList[maxV];
+    static int distances[] = new int[maxV];
+    static int fires[] = new int[maxV];
+    static int stations[] = new int[maxV];
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        int u, v, c;
+        String[] temp;
+
+        for (int t = 0; t < T; t++) {
+            temp = br.readLine().split(" ");
+            V = Integer.parseInt(temp[0]);
+            E = Integer.parseInt(temp[1]);
+            // n: 화재난 장소의 개수, m: 소방서 개수
+            n = Integer.parseInt(temp[2]);
+            m = Integer.parseInt(temp[3]);
+
+            for (int i = 0; i < V + 1; i++) {
+                if (map[i] == null)
+                    map[i] = new ArrayList<Pair>();
+                else
+                    map[i].clear();
+            }
+
+            Arrays.fill(distances, 0, V + 1, INF);
+
+            for (int i = 0; i < E; i++) {
+                temp = br.readLine().split(" ");
+                u = Integer.parseInt(temp[0]);
+                v = Integer.parseInt(temp[1]);
+                c = Integer.parseInt(temp[2]);
+                map[u].add(new Pair(v, c));
+                map[v].add(new Pair(u, c));
+            }
+
+            temp = br.readLine().split(" ");
+            for (int i = 0; i < temp.length; i++) {
+                fires[i] = Integer.parseInt(temp[i]);
+            }
+
+            temp = br.readLine().split(" ");
+            for (int i = 0; i < temp.length; i++) {
+                stations[i] = Integer.parseInt(temp[i]);
+            }
+
+            // 0 -> 소방서를 연결하고, 거리는 0 으로 한다.
+            for (int i = 0; i < m; i++) {
+                map[0].add(new Pair(stations[i], 0));
+            }
+
+            // 0 -> fires 위치 까지의 최단 거리의 합을 구한다.
+            int Answer = 0;
+
+            dijkstra();
+
+            for (int i = 0; i < n; i++) {
+                Answer += distances[fires[i]];
+            }
+
+            System.out.println(Answer);
+        }
+    }
+
+    static void dijkstra() {
+        PriorityQueue<Pair> q = new PriorityQueue<Pair>();
+        q.add(new Pair(0, 0));
+        distances[0] = 0;
+
+        while (!q.isEmpty()) {
+            int here = q.peek().node;
+            int dist_to_here = q.peek().dist;
+            q.poll();
+
+            if (distances[here] < dist_to_here)
+                continue;
+
+            for (int i = 0; i < map[here].size(); i++) {
+                int there = map[here].get(i).node;
+                int dist_here_to_there = map[here].get(i).dist;
+
+                if (distances[there] > dist_to_here + dist_here_to_there) {
+                    distances[there] = dist_to_here + dist_here_to_there;
+                    q.add(new Pair(there, distances[there]));
+                }
+            }
+        }
+    }
+
+    static class Pair implements Comparable<Pair> {
+        int node;
+        int dist;
+
+        Pair(int node, int dist) {
+            this.node = node;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Pair p) {
+            return this.dist - p.dist;
+        }
+    }
+}
---
+++ sources/30/ROUTING/Main.java
@@ -0,0 +1,104 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    static int N, M;
+    static ArrayList<Node>[] map = new ArrayList[10000];
+    static PriorityQueue<Node> q = new PriorityQueue<Node>();
+    static double[] distances = new double[10000];
+
+    static double Answer;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        int from, to;
+        double dist;
+        String[] temp;
+
+        for (int t = 0; t < T; t++) {
+            temp = br.readLine().split(" ");
+            N = Integer.parseInt(temp[0]);
+            M = Integer.parseInt(temp[1]);
+
+            for (int i = 0; i < N; i++) {
+                if (map[i] != null) {
+                    map[i].clear();
+                }
+                else {
+                    map[i] = new ArrayList<Node>();
+                }
+            }
+
+            Arrays.fill(distances, Double.MAX_VALUE);
+            q.clear();
+
+            for (int i = 0; i < M; i++) {
+                temp = br.readLine().split(" ");
+                from = Integer.parseInt(temp[0]);
+                to = Integer.parseInt(temp[1]);
+                dist = Double.parseDouble(temp[2]);
+
+                map[from].add(new Node(to, dist));
+                map[to].add(new Node(from, dist));
+            }
+
+            dijkstra();
+
+            System.out.format("%.10f", distances[N - 1]);
+            System.out.println();
+        }
+    }
+
+    static void dijkstra() {
+        q.add(new Node(0, 1.0));
+        distances[0] = 1.0;
+
+        while (!q.isEmpty()) {
+            int here = q.peek().node;
+            double dist_to_here = q.peek().dist;
+
+            q.poll();
+
+            if (distances[here] < dist_to_here || map[here] == null)
+                continue;
+
+            if (here == N - 1)
+                break;
+
+            for (int i = 0; i < map[here].size(); i++) {
+                int there = map[here].get(i).node;
+                double dist_here_to_there = map[here].get(i).dist;
+
+                if (distances[there] > dist_to_here * dist_here_to_there) {
+                    distances[there] = dist_to_here * dist_here_to_there;
+                    q.add(new Node(there, distances[there]));
+                }
+            }
+        }
+    }
+
+    static class Node implements Comparable<Node> {
+        public int node;
+        public double dist;
+
+        Node(int node, double dist) {
+            this.node = node;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Node n) {
+            if (this.dist > n.dist)
+                return 1;
+            else
+                return -1;
+        }
+    }
+}
