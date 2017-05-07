01e8934679bb3ec46a012e3a741ef80b86079f52
Donald <Donald@Donalds-MacBook-Pro.local>
2017-05-08 00:42:25 +0900

    P11266 문제풀이 수정

--- BOJ/P11266.cpp
+++ BOJ/P11266.cpp
@@ -1,60 +1,70 @@
-#include <iostream>
 #include <cstdio>
 #include <vector>
 #include <algorithm>
 using namespace std;
-#define MAXV 10010
-int V, E, counter=0, discovered[MAXV];
-bool isCutVertex[MAXV];
-vector<int> graph[MAXV];
 
-int dfs(int A, bool isRoot){
-    discovered[A]= ++counter;
-    int ret = discovered[A];
+const int MaxV = 10001;
 
-    int child=0; //정점 A가 루트 노드 일 경우를 대비해서 DFS스패닝 트리에서의 자식 수 세어준다.
-    for(int i = 0 ;  i< (int)graph[A].size() ; i++){
-        int next = graph[A][i];
-        if(!discovered[next]){
-            child++;
-            //low : 정점 A의 자식 노드가 갈 수 있는 노드중 가장 일찍 방문한 노드
-            int low = dfs(next, false);
-            if(!isRoot && low >= discovered[A])
-                isCutVertex[A] = true;
-            ret=  min(ret, low);
-        }else{
-            ret = min(ret, discovered[next]);
-        }
+int V, E, counter = 1, cutVertexCnt = 0;
+vector<int> adj[MaxV];
+int discovered[MaxV];
+bool isCutVertex[MaxV];
+
+int dfs(int here, bool isRoot);
+
+int main() {
+    freopen("/Users/Donald/Documents/Algorithm-Solving/resources/input.in", "r", stdin);
+
+    scanf("%d %d", &V, &E);
+
+    int u, v;
+    for (int i = 0; i < E; ++i) {
+        scanf("%d %d", &u, &v);
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
 
-    if(isRoot)
-        isCutVertex[A] = (child >=2);
-    return ret;
-}
-
-int main(){
-    scanf("%d%d", &V, &E);
-    for(int i =1 ; i<= E; i++){
-        int a, b;
-        scanf("%d%d", &a, &b);
-        graph[a].push_back(b);
-        graph[b].push_back(a);
-    }
-
-    for(int i = 1; i <= V; i++){
-        if(!discovered[i])
+    for (int i = 1; i <= V; ++i) {
+        if (discovered[i] == 0)
             dfs(i, true);
     }
 
-    int cnt=0;
-    for(int i = 1 ; i<= V; i++){
-        if(isCutVertex[i])
-            cnt++;
-    }
-    printf("%d\n", cnt);
-    for(int i =1 ; i<= V; i++){
-        if(isCutVertex[i])
+    printf("%d\n", cutVertexCnt);
+    for (int i = 1; i <= V; ++i) {
+        if (isCutVertex[i])
             printf("%d ", i);
     }
+
     return 0;
+}
+
+int dfs(int here, bool isRoot) {
+    discovered[here] = counter++;
+    int ret = discovered[here];
+    int childCnt = 0;
+
+    for (int i = 0; i < adj[here].size(); ++i) {
+        int there = adj[here][i];
+
+        if (discovered[there] == 0) {
+            childCnt++;
+            int subRet = dfs(there, false);
+            ret = min(ret, subRet);
+
+            if (!isRoot && !isCutVertex[here] && subRet >= discovered[here]) {
+                cutVertexCnt++;
+                isCutVertex[here] = true;
+            }
+        }
+        else {
+            ret = min(ret, discovered[there]);
+        }
+    }
+
+    if (isRoot && childCnt >= 2) {
+        cutVertexCnt++;
+        isCutVertex[here] = true;
+    }
+
+    return ret;
 }
